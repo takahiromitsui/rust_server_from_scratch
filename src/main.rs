637cdab43f21c5011333fe2312c1d0e13f80854d
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2023-02-23 11:16:58 +0100

    Write a response

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
-use std::net::TcpListener;
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+    result,
+};
 
 fn main() {
     let address = "127.0.0.1:5000";
-    // 127.0.0.0.1 is the local host
+    // 127.0.0.1 is the local host
     // bind is equivalent to new
     let listener = TcpListener::bind(address).unwrap();
     for stream in listener.incoming() {
-        let _stream = stream.unwrap();
+        let stream = stream.unwrap();
+        handle_connection(stream);
         println!("Connection established!");
     }
+}
 
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let _http_request = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect::<Vec<String>>();
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write_all(response.as_bytes()).unwrap();
 }
