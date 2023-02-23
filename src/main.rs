626f15135dd81d4b8e1a180effd3ea4be5e6ddf2
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2023-02-23 11:47:57 +0100

    Server a html file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{
+    fs,
     io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
-    result,
 };
 
 fn main() {
@@ -18,11 +18,14 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    let _http_request = buf_reader
+    let _http_request: Vec<_> = buf_reader
         .lines()
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
-        .collect::<Vec<String>>();
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+        .collect();
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
     stream.write_all(response.as_bytes()).unwrap();
 }
