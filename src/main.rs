02b57cd77eaa2810aa9e4b0e1fe7a8c1daeec49e
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2023-03-02 15:10:24 +0100

    Create MyTcpListner

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,43 @@
 use std::{
     fs,
     io::{BufRead, BufReader, Write},
-    net::{TcpListener, TcpStream},
+    net::{TcpListener, TcpStream, SocketAddr}, os::unix::prelude::{RawFd, AsRawFd}, error::Error,
 };
+
+struct MyTcpListener {
+    fd: RawFd, // raw file descriptor
+}
+
+impl MyTcpListener {
+    fn bind(addr: SocketAddr) -> Result<MyTcpListener, std::io::Error> {
+        // e.g., Inet = IPv4, Inet6 = IPv6
+        let domain = nix::sys::socket::AddressFamily::Inet;
+        // e.g., Stream = Provides sequenced, reliable, two-way, connection- based byte streams. An out-of-band data transmission mechanism may be supported.
+        let socket_type = nix::sys::socket::SockType::Stream;
+        // additional socket options
+        let flags = nix::sys::socket::SockFlag::empty();
+        // e.g., Tcp = TCP, Udp = UDP
+        let protocol = nix::sys::socket::SockProtocol::Tcp;
+        // file descriptor
+        let fd = nix::sys::socket::socket(domain, socket_type, flags, protocol).unwrap();
+
+        // bind the socket to the address
+        let addr = nix::sys::socket::SockAddr::new_inet(nix::sys::socket::InetAddr::from_std(&addr));nix::sys::socket::bind(fd, &addr).unwrap();
+        // defines the maximum number of pending connections that can be queued up before connections are refused.
+        //e.g., 10
+        nix::sys::socket::listen(fd, 10).unwrap();
+        Ok(MyTcpListener { fd })
+    }
+}
+
+
 
 fn main() {
     let address = "127.0.0.1:5000";
     // 127.0.0.1 is the local host
     // bind is equivalent to new
-    let listener = TcpListener::bind(address).unwrap();
+    // let listener = TcpListener::bind(address).unwrap();
+    let listener = MyTcpListener::bind(address.parse().unwrap()).unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         handle_connection(stream);
