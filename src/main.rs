7cac12705f8280fcc34bc85a6aac0d57d28927f8
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2023-03-02 16:02:59 +0100

    Try to add accept func

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::{
-    fs,
-    io::{BufRead, BufReader, Write},
-    net::{TcpListener, TcpStream, SocketAddr}, os::unix::prelude::{RawFd, AsRawFd}, error::Error,
+    net::{SocketAddr, IpAddr, Ipv4Addr},
+    os::unix::prelude::{RawFd},
 };
 
 struct MyTcpListener {
@@ -22,48 +21,69 @@ impl MyTcpListener {
         let fd = nix::sys::socket::socket(domain, socket_type, flags, protocol).unwrap();
 
         // bind the socket to the address
-        let addr = nix::sys::socket::SockAddr::new_inet(nix::sys::socket::InetAddr::from_std(&addr));nix::sys::socket::bind(fd, &addr).unwrap();
+        let addr =
+            nix::sys::socket::SockAddr::new_inet(nix::sys::socket::InetAddr::from_std(&addr));
+        nix::sys::socket::bind(fd, &addr).unwrap();
         // defines the maximum number of pending connections that can be queued up before connections are refused.
         //e.g., 10
         nix::sys::socket::listen(fd, 10).unwrap();
         Ok(MyTcpListener { fd })
     }
+    fn accept(&self) {
+        let mut buf = [0u8; 3000];
+        // step4: send and receive messages
+        match nix::unistd::read(self.fd, &mut buf) {
+            Ok(_val_read) => {
+                let hello = "Hello from server";
+                nix::unistd::write(self.fd, hello.as_bytes()).unwrap();
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                return;
+            }
+        };
+        // step5: close the socket
+        match nix::unistd::close(self.fd) {
+            Ok(_) => println!("Successfully closed the socket"),
+            Err(e) => println!("Error: {}", e),
+        }
+    }
 }
 
-
-
 fn main() {
-    let address = "127.0.0.1:5000";
+    // let address = "127.0.0.1:5000";
+    let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
     // 127.0.0.1 is the local host
     // bind is equivalent to new
     // let listener = TcpListener::bind(address).unwrap();
-    let listener = MyTcpListener::bind(address.parse().unwrap()).unwrap();
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        handle_connection(stream);
-        println!("Connection established!");
-    }
+    let listener = MyTcpListener::bind(socket).unwrap();
+    listener.accept();
+    // for stream in listener.incoming() {
+    //     let stream = stream.unwrap();
+    //     handle_connection(stream);
+    //     println!("Connection established!");
+    // }
 }
 
-fn handle_get_request(mut stream: TcpStream, is_success: bool, path: &str) {
-    let status_line = if is_success {
-        "HTTP/1.1 200 OK"
-    } else {
-        "HTTP/1.1 404 NOT FOUND"
-    };
-    let contents = fs::read_to_string(path).unwrap();
-    let length = contents.len();
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-    stream.write_all(response.as_bytes()).unwrap();
-}
+// fn handle_get_request(mut stream: TcpStream, is_success: bool, path: &str) {
+//     let status_line = if is_success {
+//         "HTTP/1.1 200 OK"
+//     } else {
+//         "HTTP/1.1 404 NOT FOUND"
+//     };
+//     let contents = fs::read_to_string(path).unwrap();
+//     let length = contents.len();
+//     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+//     stream.write_all(response.as_bytes()).unwrap();
+// }
 
-fn handle_connection(stream: TcpStream) {
-    let buf_reader = BufReader::new(&stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+// fn handle_connection(stream: TcpStream) {
+//     let buf_reader = BufReader::new(&stream);
+//     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    if request_line == "GET / HTTP/1.1" {
-        handle_get_request(stream, true, "hello.html")
-    } else {
-        handle_get_request(stream, false, "404.html")
-    }
-}
+//     if request_line == "GET / HTTP/1.1" {
+//         handle_get_request(stream, true, "hello.html")
+//     } else {
+//         handle_get_request(stream, false, "404.html")
+//     }
+// }
