26a85ca4342395b4addd0db573e5f56928a51d5b
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2023-04-05 17:15:17 +0200

    feat: Replace MyTcpListener and MyTcpStream with std libraries

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
+use std::io::{BufRead, BufReader, Read, Write};
+use std::net::TcpStream;
 use std::sync::{mpsc, Arc, Mutex};
 use std::{net::SocketAddr, os::unix::prelude::RawFd};
 
 use std::{io, thread};
 use serde::{Serialize, Deserialize};
+use serde_json::Value;
 
 #[derive(Serialize, Deserialize)]
 pub struct Message {
@@ -18,10 +21,15 @@ impl MyTcpStream {
     pub fn new(fd: RawFd) -> Self {
         Self { fd }
     }
-
+    // nix::unistd::read can be error prone
+    // read may return before it has filled the entire buffer if it receives a signal. In that case, you may need to call read again with the remaining buffer to get the rest of the data.
+    // read can block indefinitely if there is no data available to be read, which can be a problem in some cases.
     pub fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        println!("Reading from stream...{}", buf.len());
+        println!("self.fd: {}", self.fd);
         let n =
             nix::unistd::read(self.fd, buf).map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+        println!("Read {:?} bytes from stream", n);
         Ok(n)
     }
 
@@ -83,73 +91,124 @@ impl MyTcpListener {
     }
 
     pub fn serve_html(
-        stream: &mut MyTcpStream,
+        mut stream: TcpStream,
         root: &str,
-    ) -> Result<(), std::io::Error> {
-        let mut buffer = [0; 1024];
-        let val_read = stream.read(&mut buffer);
-        let request = String::from_utf8_lossy(&buffer[..val_read.unwrap()]);
-        let request_lines: Vec<&str> = request.lines().collect();
-        let request_line = request_lines[0];
-        let tokens: Vec<&str> = request_line.split_whitespace().collect();
-        //
-        let headers = request_lines[1..].to_vec();
-        let mut content_length: Option<usize> = None;
-        for header in headers {
-            let parts: Vec<&str> = header.splitn(2, ": ").collect();
-            if parts.len() == 2 && parts[0].to_lowercase() == "content-length" {
-                content_length = Some(parts[1].parse().unwrap());
-                println!("{:?}", content_length);
-                break;
+    )  {
+        // default buffer is 8KB
+        let buf_reader = io::BufReader::new(&mut stream);
+        // println!("{:?}", buf_reader);
+        let headers:Vec<_>= buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+        println!("headers: {:?}", headers);
+
+        let method = headers[0].split_whitespace().nth(0).unwrap();
+        let path = headers[0].split_whitespace().nth(1).unwrap();
+        println!("method: {}", method);
+        println!("path: {}", path);
+
+        if method == "GET" {
+            let mut file_path = root.to_string();
+            if path == "/" {
+                file_path.push_str("/index");
+            } else {
+                file_path.push_str(path);
             }
+
+            let file = std::fs::read_to_string(format!("{}.html", file_path));
+            let not_found = std::fs::read_to_string(format!("{}/404.html", root));
+        // // write back to the new socket
+            let response = match file {
+                Ok(body) => {
+                    format!(
+                        "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: {}\n\n{}",
+                        body.len(),
+                        body
+                    )
+                }
+                Err(_) => {
+                    let body = match not_found {
+                        Ok(body) => body,
+                        Err(_) => "404 Not Found".to_string(),
+                    };
+                    format!(
+                        "HTTP/1.1 404 Not Found\nContent-Type: text/html\nContent-Length: {}\n\n{}",
+                        body.len(),
+                        body
+                    )
+                }
+            };
+            stream.write_all(response.as_bytes()).unwrap();
+        } else if method == "POST" {
+            // let stream = stream.try_clone().unwrap();
+            // Self::post_json(*buf_reader, path, Self::update_json)
+        } else {
+            println!("Method not supported");
         }
-        let content_length = content_length.unwrap_or(0);
-        if content_length> buffer.len() {
-            println!("Buffer is not enough")
-        }
+        // let mut buffer = [0; 1024];
+        // let val_read = stream.read(&mut buffer);
+        // let request = String::from_utf8_lossy(&buffer[..val_read.unwrap()]);
+        // let request_lines: Vec<&str> = request.lines().collect();
+        // let request_line = request_lines[0];
+        // let tokens: Vec<&str> = request_line.split_whitespace().collect();
+        // //
+        // let headers = request_lines[1..].to_vec();
+        // let mut content_length: Option<usize> = None;
+        // for header in headers {
+        //     let parts: Vec<&str> = header.splitn(2, ": ").collect();
+        //     if parts.len() == 2 && parts[0].to_lowercase() == "content-length" {
+        //         content_length = Some(parts[1].parse().unwrap());
+        //         println!("{:?}", content_length);
+        //         break;
+        //     }
+        // }
+        // let content_length = content_length.unwrap_or(0);
+        // if content_length> buffer.len() {
+        //     println!("Buffer is not enough")
+        // }
 
     
-        // get the requested file path from the URL
-        let (file_path, is_post) = if tokens[1] == "/" {
-            ("/index", false)
-        } else {
-            let mut parts = tokens[1].splitn(2, '?');
-            let file_path = parts.next().unwrap();
-            let is_post = parts.next().map_or(false, |p| p == "post=true");
-            (file_path, is_post)
-        };
-        // println!("{} {}", tokens[0], tokens[1]);
-        if tokens[0] == "POST" && tokens[1] == "/message" {
-            println!("POST /message is called");
-            Self::post_json(stream, "/message", Self::update_json);
-            return Ok(())
-        }
+        // // get the requested file path from the URL
+        // let (file_path, is_post) = if tokens[1] == "/" {
+        //     ("/index", false)
+        // } else {
+        //     let mut parts = tokens[1].splitn(2, '?');
+        //     let file_path = parts.next().unwrap();
+        //     let is_post = parts.next().map_or(false, |p| p == "post=true");
+        //     (file_path, is_post)
+        // };
+        // if tokens[0] == "POST" && tokens[1] == "/message" {
+        //     Self::post_json(stream, "/message", Self::update_json);
+        //     return Ok(())
+        // }
     
-        let file = std::fs::read_to_string(format!("{}{}.html", root, file_path));
-        let not_found = std::fs::read_to_string(format!("{}/404.html", root));
-        // write back to the new socket
-        let response = match file {
-            Ok(body) => {
-                format!(
-                    "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: {}\n\n{}",
-                    body.len(),
-                    body
-                )
-            }
-            Err(_) => {
-                let body = match not_found {
-                    Ok(body) => body,
-                    Err(_) => "404 Not Found".to_string(),
-                };
-                format!(
-                    "HTTP/1.1 404 Not Found\nContent-Type: text/html\nContent-Length: {}\n\n{}",
-                    body.len(),
-                    body
-                )
-            }
-        };
-        stream.write(response.as_bytes())?;
-        Ok(())
+        // let file = std::fs::read_to_string(format!("{}{}.html", root, file_path));
+        // let not_found = std::fs::read_to_string(format!("{}/404.html", root));
+        // // write back to the new socket
+        // let response = match file {
+        //     Ok(body) => {
+        //         format!(
+        //             "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: {}\n\n{}",
+        //             body.len(),
+        //             body
+        //         )
+        //     }
+        //     Err(_) => {
+        //         let body = match not_found {
+        //             Ok(body) => body,
+        //             Err(_) => "404 Not Found".to_string(),
+        //         };
+        //         format!(
+        //             "HTTP/1.1 404 Not Found\nContent-Type: text/html\nContent-Length: {}\n\n{}",
+        //             body.len(),
+        //             body
+        //         )
+        //     }
+        // };
+        // stream.write(response.as_bytes())?;
+        // Ok(())
     }
 
     pub fn update_json(body: &str) -> Result<Message, String> {
@@ -157,43 +216,86 @@ impl MyTcpListener {
     }
    
 
-    pub fn post_json<F>(stream: &mut MyTcpStream, path: &str, handler: F)
-    where
-        F: Fn(&str) -> Result<Message, String> + Send + Sync + 'static,
-    {
-            println!("post_json is called");
-            
-            let mut buffer = [0; 1024];
-            let val_read = stream.read(&mut buffer);
-            if val_read.is_err() {
-                println!("Error reading from connection: {}", val_read.err().unwrap());
-                return;
-            }
-            println!("val_read: {:?}", val_read);
-            let request = String::from_utf8_lossy(&buffer[..val_read.unwrap()]);
-            let body = request.splitn(2, "\r\n\r\n").nth(1).unwrap_or("");
-            println!("request: {}", request);
-            println!("body: {}", body);
-            match handler(body) {
-                Ok(msg) => {
-                    let response = serde_json::to_string(&msg).unwrap();
-                    let response = format!(
-                        "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: {}\r\n\r\n{}",
-                        response.len(),
-                        response
-                    );
-                    stream.write(response.as_bytes()).unwrap();
-                }
-                Err(e) => {
-                    let response = format!(
-                        "HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                        e.len(),
-                        e
-                    );
-                    stream.write(response.as_bytes()).unwrap();
-                }
-            }
+    pub fn post_json<F>(stream: TcpStream, path: &str, handler: F) {
+        println!("post_json is called");
+
+    
+        // let mut buffer = [0; 1024];
+        // let mut input = String::new();
+    
+        // loop {
+        //     match stream.read(&mut buffer) {
+        //         Ok(n) if n > 0 => {
+        //             input.push_str(std::str::from_utf8(&buffer[..n]).unwrap());
+        //             println!("input: {}", input);
+        //             if input.contains("\r\n\r\n") {
+        //                 break; // End of headers
+        //             }
+        //         }
+        //         Ok(_) => break, // End of stream
+        //         Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+        //             // Wait for more data
+        //             thread::sleep(std::time::Duration::from_millis(10));
+        //         }
+        //         Err(e) => {
+        //             eprintln!("Error reading from stream: {}", e);
+        //             return;
+        //         }
+        //     }
+        // }
+    
+        // println!("input: {}", input);
+    
+        // let response = match serde_json::from_str::<Value>(&input) {
+        //     Ok(json) => serde_json::to_string(&json).unwrap(),
+        //     Err(e) => format!("{{ \"error\": \"{}\" }}", e),
+        // };
+        // let response = format!(
+        //     "HTTP/1.1 200 OK\nContent-Type: application/json\nContent-Length: {}\n\n{}",
+        //     response.len(),
+        //     response
+        // );
+        // stream.write(response.as_bytes()).unwrap();
     }
+    
+    
+    
+    // where
+    //     F: Fn(&str) -> Result<Message, String> + Send + Sync + 'static,
+    // {
+    //         println!("post_json is called");
+            
+    //         let mut buffer = [0; 1024];
+    //         let val_read = stream.read(&mut buffer);
+    //         if val_read.is_err() {
+    //             println!("Error reading from connection: {}", val_read.err().unwrap());
+    //             return;
+    //         }
+    //         println!("val_read: {:?}", val_read);
+    //         let request = String::from_utf8_lossy(&buffer[..val_read.unwrap()]);
+    //         let body = request.splitn(2, "\r\n\r\n").nth(1).unwrap_or("");
+    //         println!("request: {}", request);
+    //         println!("body: {}", body);
+    //         match handler(body) {
+    //             Ok(msg) => {
+    //                 let response = serde_json::to_string(&msg).unwrap();
+    //                 let response = format!(
+    //                     "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: {}\r\n\r\n{}",
+    //                     response.len(),
+    //                     response
+    //                 );
+    //                 stream.write(response.as_bytes()).unwrap();
+    //             }
+    //             Err(e) => {
+    //                 let response = format!(
+    //                     "HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+    //                     e.len(),
+    //                     e
+    //                 );
+    //                 stream.write(response.as_bytes()).unwrap();
+    //             }
+    //         }
+    // }
     
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
+use std::net::TcpListener;
+
 use rust_server::{MyTcpListener, ThreadPool};
 
 
 fn main() -> std::io::Result<()> {
     let addr = "127.0.0.1:8080";
-    let listener = MyTcpListener::bind(addr.parse().unwrap())?;
+    let listener = TcpListener::bind(addr)?;
+    // let listener = MyTcpListener::bind(addr.parse().unwrap())?;
     let pool = ThreadPool::new(4);
+
     loop {
-        let mut stream = match listener.accept() {
+        let stream = match listener.accept() {
             Ok(stream) => stream,
             Err(e) => {
                 println!("Error accepting connection: {}", e);
@@ -14,7 +18,19 @@ fn main() -> std::io::Result<()> {
             }
         };
         pool.execute(move || {
-            MyTcpListener::serve_html(&mut stream, "src/views").unwrap();
+            MyTcpListener::serve_html(stream.0, "src/views");
         })
     }
+    // loop {
+    //     let mut stream = match listener.accept() {
+    //         Ok(stream) => stream,
+    //         Err(e) => {
+    //             println!("Error accepting connection: {}", e);
+    //             continue;
+    //         }
+    //     };
+    //     pool.execute(move || {
+    //         MyTcpListener::serve_html(&mut stream, "src/views").unwrap();
+    //     })
+    // }
 }
